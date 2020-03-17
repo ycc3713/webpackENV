c5d8508dd6aa38e92c660687bbd330ea27cedb6e
yangcongcong <ycc3713@163.com>
2020-03-17 16:15:11 +0800

    起步。

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var a = 123
+console.log(123)
+var b = 789
+var c = a + b
+console.log(c)
+
+var element = document.createElement('div')
+element.innerText = 'hello world'
+document.body.appendChild(element)
